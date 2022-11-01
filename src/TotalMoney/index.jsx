8cff5edca4fbbf87b30cb6f46fdebf3bdb43af3e
Kawã <kawablima03@hotmail.com>
2022-11-01 15:56:48 -0300

    atualização

--- src/TotalMoney/index.jsx
+++ src/TotalMoney/index.jsx
@@ -1,9 +1,9 @@
 import './style.css';
 
 const TotalMoney = ({ listTransaction }) => {
-  console.log(listTransaction)
   const valorTotal = listTransaction.reduce((valorInicial, valorAtual) => 
     valorAtual.type === 'despesa' ? valorInicial - Number(valorAtual.value) : valorInicial + Number(valorAtual.value), 0) 
+    
     return (
       <div className='mainTM'>
       <div className='valueTotal'>
